afc83ce0e767dc1d2b22f1006fba7c5ba2acef66
fauzi1999mei <86035007+fauzi1999mei@users.noreply.github.com>
2021-08-27 04:12:10 +0800

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,32 @@
+//console.log("hello fauzi")
+
+let myImg = document.querySelector('img');
+
+myImg.onclick =function(){
+  let mySrc = myImg.getAttribute('src');
+    if(mySrc === "images/Capture.JPG"){
+       myImg.setAttribute('src', "images/Capture2.JPG");
+   }
+   else if(mySrc === "images/Capture2.JPG"){
+       myImg.setAttribute('src', "images/Capture3.JPG");
+   }
+    else{
+       myImg.setAttribute('src', "images/Capture.JPG");
+   }
+}
+
+//let btn1 = document.querySelector('#btn1');
+//let btn2 = document.querySelector('#btn2');
+//btn1.addEventListener('click' , () => {
+//    img.src = 'images/Capture.JPG';
+//})
+//btn2.addEventListener('click', () => {
+ //   img.src = 'images/Capture2.JPG';
+//})
+//function btn(){
+ //  let image = document.getElementById('img');
+ //   let images = ['images/Capture.JPG','images/Capture2.JPG'];
+
+ //   let random = Math.floor(Math.random() * 2);
+ //image.src = images[random];
+//}
